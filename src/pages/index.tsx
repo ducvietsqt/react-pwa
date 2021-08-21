ac62d7fa8a15b05998bc9b41c5c5ddfa1ac55e55
hoducviet <ducvietsqt@gmail.com>
2021-08-21 10:13:52 +0700

    remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import HelloWorld from "../components/HelloWorld"
-
 export default class Home extends React.Component {
-  componentDidMount() {
-    // todo
-  }
-
   render() {
     return (
-      <HelloWorld/>
+      <div>
+        Hello World
+      </div>
     )
   }
 }
